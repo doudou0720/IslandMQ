13ed2d391bd2709def3d6ab6b5cc8807f6f88226
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2026-06-05 19:01:46 +0800

    refactor: 将项目代码迁移为.sln架构
    Signed-off-by: doudou0720 <98651603+doudou0720@users.noreply.github.com>

---
+++ IslandMQ.Core/Settings/BasicSettingsControl.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+
+namespace IslandMQ.Settings;
+
+/// <summary>
+/// 基本设置内容控件
+/// </summary>
+public partial class BasicSettingsControl : UserControl
+{
+    /// <summary>
+    /// 初始化 BasicSettingsControl 实例
+    /// </summary>
+    public BasicSettingsControl()
+    {
+        InitializeComponent();
+    }
+}
